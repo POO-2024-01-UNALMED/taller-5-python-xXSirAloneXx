c7d0610378ad3499aca023b2c795b4bb4f40e07a
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-19 11:33:30 -0500

    5

--- zooAnimales/Pez.py
+++ zooAnimales/Pez.py
@@ -1,19 +1,41 @@
-from zooAnimales.Animal import Animal
+from zooAnimales.animal import Animal
 
 class Pez(Animal):
+    _listado = []
+    salmones = 0
+    bacalaos = 0
 
-    def __init__(self, Pez, salmones, bacalaos, colorEscamas, cantidadAletas):
-        self._listado = Pez[]
-        self.salmones = salmones
-        self.bacalaos = bacalaos
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
+        super().__init__(nombre, edad, habitat, genero)
         self._colorEscamas = colorEscamas
+        self._cantidadAletas =cantidadAletas
+        Pez._listado.append(self)
+
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setColorEscamas(self, colorEscamas):
+        self._colorEscamas = colorEscamas
+    
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+    
+    def setCantidadAletas(self, cantidadAletas):
         self._cantidadAletas = cantidadAletas
 
-    def cantidadPeces(self):
+    def movimiento():
+        return "nadar"
     
-    def movimiento(self):
-
-    def crearSalmon(self):
-
-    def crearBacalao(self):
+    @staticmethod
+    def cantidadPeces():
+        return len(Pez._listado)
     
+    @classmethod
+    def crearSalmon(cls, nombre, edad, genero):
+        Pez(nombre, edad, "oceano", genero, "rojo", 6)
+        cls.salmones += 1
+
+    @classmethod
+    def crearBacalao(cls, nombre, edad, genero):
+        Pez(nombre, "oceano", genero, "gris", 6)
+        cls.bacalaos += 1
