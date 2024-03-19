0a1fb65df20cc27bea6a2c513526c448a27c2d23
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-19 11:52:42 -0500

    1

--- gestion/Zoologico.py
+++ gestion/Zoologico.py
@@ -1,11 +1,32 @@
 class Zoologico:
+    _zonas = []
 
-    def __init__(self, nombre, ubicacion, zonas[]):
+    def __init__(self, nombre, ubicacion):
         self._nombre = nombre
         self._ubicacion = ubicacion
-        self._zonas = zonas
 
-    def agregarZonas(self):
+    def getNombre(self):
+        return self._nombre
     
-    def cantidadTotalAnimales(self):
-        
+    def setNombre(self, nNombre):
+        self._nombre = nNombre
+
+    def getUbicacion(self):
+        return self._ubicacion
+    
+    def setUbicacion(self, nUbicacion):
+        self._ubicacion = nUbicacion
+
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+
+    @classmethod
+    def setZona(cls, zonas):
+        cls._zonas = zonas
+
+    def cantidadTitalAnimales(self):
+        cantidadA = 0
+        for zona in self._zonas:
+            cantidadA += zona.cantidadAnimales()
+        return cantidadA
+    
