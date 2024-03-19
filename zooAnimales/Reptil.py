c3faadac2a02dade6a6132c616cce14407c4df16
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-19 11:44:22 -0500

    1

--- zooAnimales/Reptil.py
+++ zooAnimales/Reptil.py
@@ -1,18 +1,41 @@
-from zooAnimales.Animal import Animal
+from zooAnimales.animal import Animal
 
 class Reptil(Animal):
+    _listado = []
+    iguanas = 0
+    serpientes = 0
 
-    def __init__(self, Reptil, iguanas, serpientes, colorEscamas, largoCola):
-        self._listado = Reptil[]
-        self.iguanas = iguanas
-        self.serpientes = serpientes
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
+        super().__init__(nombre, edad, habitat, genero)
         self._colorEscamas = colorEscamas
         self._largoCola = largoCola
+        Reptil._listado.append(self)
 
-    def cantidadReptiles(self):
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setColorEscamas(self, colorEscamas):
+        self._colorEscamas = colorEscamas
 
-    def movimiento(self):
+    def getLargoCola(self):
+        return self._largoCola
+    
+    def setLargoCola(self, largoCola):
+        self._largoCola = largoCola
 
-    def crearIguana(self):
+    def movimiento():
+        return "reptar"
+    
+    @staticmethod
+    def cantidadReptiles():
+        return len(Reptil._listado)
+    
+    @classmethod
+    def crearIguana(cls, nombre, edad, genero):
+        Reptil(nombre, edad, "humedal", genero, "verde", 3)
+        cls.iguanas += 1
 
-    def crearSerpiente(self):
+    @classmethod
+    def crearSerpiente(cls, nombre, edad, genero):
+        Reptil(nombre, edad, "jungla", genero, "blanco", 3)
+        cls.serpientes += 1
