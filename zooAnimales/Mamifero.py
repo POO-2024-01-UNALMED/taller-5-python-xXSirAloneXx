fafe2a5d304e59c746e2f0394a9ed175f9ac6fca
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-19 11:22:50 -0500

    4

--- zooAnimales/Mamifero.py
+++ zooAnimales/Mamifero.py
@@ -1,21 +1,40 @@
-from zooAnimales.Animal import Animal
+from zooAnimales.animal import Animal
 
 class Mamifero(Animal):
-
+    _listado = []
     caballos = 0
     leones = 0
-    _listado = []
-    def __init__(self, Mamifero, caballos, leones, pelaje, patas):
-        self._listado = Mamifero[]
-        self.caballos = caballos
-        self.leones = leones
+
+    def __init__(self, nombre, edad, habitat, genero, pelaje, patas):
+        super().__init__(nombre, edad, habitat, genero)
         self._pelaje = pelaje
         self._patas = patas
+        Mamifero._listado.append(self)
 
-    def cantidadMamiferos(self):
+    def isPelaje(self):
+        return self._pelaje
     
-    def crearCaballo(self):
+    def setPelaje(self, pelaje):
+        self._pelaje = pelaje
     
-    def crearLeon(self):
+    def getPatas(self):
+        return self._patas
     
+    def setPatas(self, patas):
+        self._patas = patas
 
+    @staticmethod
+    def cantidadMamiferos():
+        return len(Mamifero._listado)
+    
+    @classmethod
+    def crearCaballo(cls, nombre, edad, genero):
+        Mamifero(nombre, edad, "pradera", genero, True, 4)
+        cls.caballos += 1
+
+    @classmethod
+    def crearLeon(cls, nombre, edad, genero):
+        Mamifero(nombre, edad, "selva", genero, True, 4)
+        cls.leones += 1
+
+    
